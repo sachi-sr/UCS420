cb9b5239982d008da678f0a3e4251978fba64115
Sachi <sachisri05@gmail.com>
2026-08-07 14:38:32 +0530

    Module 3

---
+++ Module3.py
@@ -0,0 +1,12 @@
+#3.1
+n=input("First name: ")
+s=input("Surname: ")
+f=n+" "+s
+print("full name: "+f)
+
+a=int(input("Enter number: "))
+b=int(input("Enter number:"))
+c=a+b
+print(a,"+",b,"=",c)
+
+
