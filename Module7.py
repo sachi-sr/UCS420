4608171791d985cb1489b5fbdb3e14518d747dd9
Sachi <sachisri05@gmail.com>
2026-08-21 14:40:43 +0530

    Create Module7.py

---
+++ Module7.py
@@ -0,0 +1,11 @@
+import math as m
+print ("exp(-200)    --> ", m.exp(-200))  # Exponential function
+print ("log(100,2)   --> ", m.log(100,2)) # Log
+print ("log(100,10)  --> ", m.log(100,10))# Log
+print ("log10(100)   --> ", m.log10(100)) # Log 10
+print ("m.cos(30)    --> ", m.cos(30))    # cos
+print ("m.sin(30)    --> ", m.sin(30))    # sin
+print ("m.tan(30)    --> ", m.tan(30))    # tan
+print ("m.sqrt(324)  --> ", m.sqrt(324))
+print ("m.ceil(89.9) --> ", m.ceil(89.9))
+print ("m.floor(89.9)--> ", m.floor(89.9))
