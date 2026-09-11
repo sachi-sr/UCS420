125c40820c7147d492d81d8d9bc72841db621d22
Sachi <sachisri05@gmail.com>
2026-09-11 14:29:19 +0530

    Assignment2 Q3

--- A2_3.py
+++ A2_3.py
@@ -1,42 +1,40 @@
 import random
 random.seed(1024170398)
-numbers=[]
-for i in range (100):
-  number = random.randint(100,900)
-  number.append(number)
+numbers = []
+for i in range(100):
+    number = random.randint(100, 900)
+    numbers.append(number)
 
 print("Random numbers:")
 print(numbers)
 
-odd_number=[]
+odd_number = []
 for number in numbers:
-  if number%2 != 0:
-    odd_number.append(number)
+    if number % 2 != 0:
+        odd_number.append(number)
 
-print("Number of odd numbers:",len(odd_number))
-print("Odd numbers:",odd_number)
+print("Number of odd numbers:", len(odd_number))
+print("Odd numbers:", odd_number)
 
-evenno=[]
-for nubmer in numbers:
-  if number%2==0:
-    evenno.append(number)
-
-print("Number of even numbers:",len(evenno))
-print("Even numbers:",evenno)
-
-primeno=[]
+evenno = []
 for number in numbers:
-  if nubmer>1:
-    is_prime=True
+    if number % 2 == 0:
+        evenno.append(number)
 
-    for i in range(2,number):
-      if number %i ==0:
-         is_prime=False
-         break
-    if is_prime:
-      primeno.append(number)
+print("Number of even numbers:", len(evenno))
+print("Even numbers:", evenno)
 
-print("number of prime nubmers:",len(primeono))
-print("Prime numbers:",primeno)
+primeno = []
+for number in numbers:
+    if number > 1:
+        is_prime = True
 
-prime
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primeno.append(number)
+
+print("Number of prime numbers:", len(primeno))
+print("Prime numbers:", primeno)
